a91f856937ac0ca36e319eafc05f12b13e97b5e8
jabu <jaburu@dakimakuri.com>
2019-01-09 22:06:01 -0600

    Add Japanese i18n support

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, TRANSLATIONS, TRANSLATIONS_FORMAT } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -8,5 +8,17 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+// change development locale (en or ja)
+const locale: string = 'en';
+
+// use the require method provided by webpack
+declare const require;
+// we use the webpack raw-loader to return the content as a string
+const translations = environment.production ? null : (locale == 'en' ? null : require(`raw-loader!./i18n/messages.${locale}.xlf`));
+
+platformBrowserDynamic().bootstrapModule(AppModule, {
+  providers: [
+    { provide: TRANSLATIONS, useValue: translations },
+    { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' }
+  ]
+});
