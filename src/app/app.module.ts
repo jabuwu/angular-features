3480b588311eca0fa10d8091c0db74cc11779047
jabu <jaburu@dakimakuri.com>
2019-01-09 22:21:33 -0600

    Add Angular Universal support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({appId: 'myapp'})
   ],
   providers: [],
   bootstrap: [AppComponent]
